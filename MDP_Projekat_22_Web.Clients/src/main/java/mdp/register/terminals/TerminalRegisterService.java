578b067d074e283e3e8d2b1818c1c25f53de8e4c
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2022-07-30 21:18:38 +0200

    Generated SOAP client.

---
+++ MDP_Projekat_22_Web.Clients/src/main/java/mdp/register/terminals/TerminalRegisterService.java
@@ -0,0 +1,18 @@
+/**
+ * TerminalRegisterService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package mdp.register.terminals;
+
+public interface TerminalRegisterService extends java.rmi.Remote {
+    public mdp.register.terminals.dtos.GetCustomsTerminalDto[] getTerminalsStartingWithName(java.lang.String namePrefix) throws java.rmi.RemoteException;
+    public mdp.register.terminals.dtos.GetCustomsTerminalDto createTerminal(mdp.register.terminals.dtos.CreateTerminalDto dto) throws java.rmi.RemoteException;
+    public mdp.register.terminals.dtos.GetCustomsTerminalDto[] getTerminals() throws java.rmi.RemoteException;
+    public void deleteTerminal(java.math.BigInteger id) throws java.rmi.RemoteException, mdp.exceptions.TerminalNotFoundException;
+    public mdp.register.terminals.dtos.GetCustomsTerminalDto getTerminal(java.math.BigInteger passageId, boolean isCustomsStep, java.lang.String terminalName) throws java.rmi.RemoteException, mdp.exceptions.TerminalNotFoundException;
+    public mdp.register.terminals.dtos.GetCustomsTerminalDto searchTerminal(mdp.dtos.SearchTerminalDto dto) throws java.rmi.RemoteException, mdp.exceptions.TerminalNotFoundException;
+    public mdp.register.terminals.dtos.GetCustomsTerminalDto updateTerminal(mdp.register.terminals.dtos.UpdateTerminalDto dto) throws java.rmi.RemoteException;
+}
