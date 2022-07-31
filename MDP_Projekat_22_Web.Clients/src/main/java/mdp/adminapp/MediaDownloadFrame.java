b89b78fcef630ab02f6cb22735ad50da5f9d8e26
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2022-07-31 20:39:56 +0200

    Refactor of downloading. Added person documents downloading.

--- MDP_Projekat_22_Web.Clients/src/main/java/mdp/adminapp/MediaDownloadFrame.java
+++ MDP_Projekat_22_Web.Clients/src/main/java/mdp/adminapp/MediaDownloadFrame.java
@@ -4,11 +4,13 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,6 +20,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import mdp.util.Util;
@@ -35,19 +38,54 @@ public class MediaDownloadFrame {
 
 		JButton downloadWantedLogsButton = getDownloadWantedLogsButton();
 
-		JButton downloadPersonDocumentsButton = new JButton("Person Documents");
+		JButton downloadPersonDocumentsButton = getDownloadPersonDocumentsButton();
 
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		buttonPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
 		buttonPanel.setBorder(BorderFactory.createTitledBorder("Download Options"));
 		buttonPanel.setMaximumSize(new Dimension(400, 400));
-//		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
 
 		buttonPanel.add(downloadWantedLogsButton);
-		buttonPanel.add(downloadWantedLogsButton);
+		buttonPanel.add(downloadPersonDocumentsButton);
 
 		contentPane.add(buttonPanel);
+	}
+
+	private static JButton getDownloadPersonDocumentsButton() {
+		JButton button = new JButton("Person Documents");
+
+		button.addActionListener(e -> {
+			String personId = JOptionPane.showInputDialog("Insert Person ID");
+
+			File saveDirectoryFile = getSaveDirectory();
+			if (saveDirectoryFile == null)
+				return;
+
+			downloadAndSavePersonDocumentsToFile(saveDirectoryFile, personId);
+		});
+
+		return button;
+	}
+
+	private static void downloadAndSavePersonDocumentsToFile(File saveDirectoryFile, String personId) {
+		Path saveDirectoryPath = saveDirectoryFile.toPath();
+		Path saveFilePath = Paths.get(saveDirectoryPath.toString(),
+				String.format("%s_%s_person.zip", Util.getStringUuid(), personId));
+		File saveFile = saveFilePath.toFile();
+
+		URL url;
+		try {
+			url = new URL(
+					String.format("http://%s/api/documents/person/%s", Main.settings.getWantedServerHost(), personId));
+			downloadToFileFromUrl(saveDirectoryFile, saveFile, url);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private static JButton getDownloadWantedLogsButton() {
@@ -72,26 +110,7 @@ public class MediaDownloadFrame {
 		URL url;
 		try {
 			url = new URL(String.format("http://%s/api/wanted", Main.settings.getWantedServerHost()));
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-
-			connection.setRequestProperty("Content-Type", "application/octet-stream");
-
-			connection.connect();
-			int responseCode = connection.getResponseCode();
-			try (InputStream inputStream = connection.getInputStream();
-					FileOutputStream out = new FileOutputStream(saveFile)) {
-				byte[] buf = new byte[8192];
-				int length;
-				while ((length = inputStream.read(buf)) != -1) {
-					out.write(buf, 0, length);
-				}
-			}
-
-			UiUtil.showInfoMessage(frame,
-					String.format("Successfully saved %s in %s", saveFile.getName(), saveDirectoryFile),
-					"Save File Success");
-
+			downloadToFileFromUrl(saveDirectoryFile, saveFile, url);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -99,33 +118,47 @@ public class MediaDownloadFrame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		connection.inpu
+	}
 
-		// try {
+	private static void downloadToFileFromUrl(File saveDirectoryFile, File saveFile, URL url)
+			throws IOException, ProtocolException, FileNotFoundException {
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
 
-//				Client httpClient = ClientBuilder.newClient();
-//				WebTarget httpTarget = httpClient.target(Main.settings.getWantedServerHost()).path("api")
-//						.path("wanted");
-//
-//				Builder invocationBuilder = httpTarget.request(MediaType.APPLICATION_OCTET_STREAM);
-//				Response response = invocationBuilder.method("GET");
-//
-//				int responseStatus = response.getStatus();
-//				if (!isSuccessStatusCode(responseStatus))
-//					UiUtil.showErrorMessage(frame, "Error downloading wanted logs", "Wanted Logs Error",
-//							response.getStatusInfo().toString());
-//
-//				try (InputStream inStream = response.readEntity(InputStream.class)) {
-//					System.out.println();
-//				} catch (IOException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-//			} catch (Exception e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//		});
+		connection.setRequestProperty("Content-Type", "application/octet-stream");
+
+		connection.connect();
+		int responseCode = connection.getResponseCode();
+		if (!isSuccessStatusCode(responseCode)) {
+			String errorMessage = connection.getResponseMessage();
+			if (errorMessage == null || errorMessage.equals("null"))
+				if (responseCode == 404)
+					errorMessage = "Files not found";
+				else
+					errorMessage = "Generic error";
+
+			UiUtil.showErrorMessage(frame, "Download failed", "Download failed", errorMessage);
+			return;
+		}
+
+		try (InputStream inputStream = connection.getInputStream();
+				FileOutputStream out = new FileOutputStream(saveFile)) {
+			copyInStreamToOut(inputStream, out);
+		} catch (FileNotFoundException ex) {
+
+		}
+
+		UiUtil.showInfoMessage(frame,
+				String.format("Successfully saved %s in %s", saveFile.getName(), saveDirectoryFile),
+				"Save File Success");
+	}
+
+	private static void copyInStreamToOut(InputStream inputStream, FileOutputStream out) throws IOException {
+		byte[] buf = new byte[8192];
+		int length;
+		while ((length = inputStream.read(buf)) != -1) {
+			out.write(buf, 0, length);
+		}
 	}
 
 	private static File getSaveDirectory() {
